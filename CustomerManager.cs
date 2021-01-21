957696a3c365e4545d95e3bbe6ab9d6adadc773c
Neoakay005 <47914938+Neoakay005@users.noreply.github.com>
2021-01-21 22:31:18 +0300

    2021-01-21 demonun son hali

---
+++ CustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer musteri)
+        {
+            Console.WriteLine("Müşteri Id : " + musteri.Id);
+            Console.WriteLine("Adı : " + musteri.Name);
+            Console.WriteLine("Soyadı : " + musteri.Surname);
+            Console.WriteLine("Doğum Yılı : " + musteri.BirthYear);
+        }
+        public void List(Customer[] musteri)
+        {
+            foreach (Customer i in musteri)
+            {
+                Console.WriteLine("Musteri Adı, Soyadı: "+ i.Name + ", " + i.Surname);
+            }
+        }
+        public void Delete(Customer musteri)
+        {
+            Console.WriteLine("Müşteri silindi: " + musteri.Name + " " + musteri.Surname);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,38 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer musteri1 = new Customer();
+            musteri1.Id = 1;
+            musteri1.Name = "Halil";
+            musteri1.Surname = "Akay";
+            musteri1.BirthYear = 1987;
+
+            Customer musteri2 = new Customer();
+            musteri2.Id = 2;
+            musteri2.Name = "Ali";
+            musteri2.Surname = "Çalık";
+            musteri2.BirthYear = 1965;
+
+            Customer musteri3 = new Customer();
+            musteri3.Id = 3;
+            musteri3.Name = "Yavuz";
+            musteri3.Surname = "Solmaz";
+            musteri3.BirthYear = 1982;
+
+            Customer[] musteri = new Customer[] {musteri1,musteri2,musteri3};
+
+            CustomerManager musteriManager = new CustomerManager();
+            musteriManager.List(musteri);
+
+            Console.WriteLine("-----------------------------------");
+            musteriManager.Add(musteri1);
+            Console.WriteLine("-----------------------------------");
+            musteriManager.Add(musteri2);
+            Console.WriteLine("-----------------------------------");
+            musteriManager.Add(musteri3);
+            Console.WriteLine("-----------------------------------");
+            musteriManager.Delete(musteri1);
+            
         }
     }
 }
